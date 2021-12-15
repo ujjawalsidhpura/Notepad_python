89413ac03b27f7cb6b7f7f98bd18f07d41e924dd
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2021-12-15 16:42:31 +0530

    login/signup page set up and POST routes

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -14,6 +14,23 @@ def logout():
     return '<p>Logout</p>'
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 letter', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 5:
+            flash('Password must be atleast 5 characters', category='error')
+        else:
+            flash('Account created', category='success')
+
     return render_template('sign_up.html')
