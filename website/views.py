1d5150a0c136d178a3b5f0d0edcd91e8007dc73f
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-12 13:58:29 -0500

    delete a note

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required,  current_user
 from sqlalchemy.sql.functions import user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -22,3 +23,15 @@ def home():
             flash('Note added', category='success')
 
     return render_template('home.html', user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.load(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
