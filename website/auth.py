a17aff702534796c1b8aa2c80c928d42e4280391
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-12 13:02:01 -0500

    user login authentication

--- website/auth.py
+++ website/auth.py
@@ -9,6 +9,18 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', category='success')
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Email does not exist', category='error')
     return render_template('login.html')
 
 
@@ -25,7 +37,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('User already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 letter', category='error')
