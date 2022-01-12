ba78e95c2a64bd466fb18916c2a75a16133f0ae5
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-12 12:47:12 -0500

    create user

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 auth = Blueprint('auth', __name__)
@@ -31,6 +34,10 @@ def sign_up():
         elif len(password1) < 5:
             flash('Password must be atleast 5 characters', category='error')
         else:
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', category='success')
-
+            return redirect(url_for('views.home'))
     return render_template('sign_up.html')
