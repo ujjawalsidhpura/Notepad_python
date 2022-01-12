bf73d8c27cfc438b1da2d3d563e4ea36f1279616
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-12 12:20:43 -0500

    create relationship between tables

--- website/models.py
+++ website/models.py
@@ -8,6 +8,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
@@ -15,3 +16,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
